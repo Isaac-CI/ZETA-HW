3476532eefb31f480d169839428a99be4d8a3c5d
Isaac-CI <isaacnmarinho@gmail.com>
2025-06-19 03:10:28 -0300

    Remove latches inferidos da operação de imagem linear

--- RTL/CZonotope/linear_image.sv
+++ RTL/CZonotope/linear_image.sv
@@ -19,6 +19,7 @@ module linear_image #(
   logic [$clog2(NMAX)-1:0] itrn;
   logic [$clog2(NRMAX)-1:0] itrr;
   logic [$clog2(NGMAX)-1:0] itrg;
+  logic [$clog2(NGMAX)-1:0] r_itrg;
 
   logic[DATA_WIDTH+1:0] Zc, Rn, Zg; 
 
@@ -27,21 +28,82 @@ module linear_image #(
   logic [DATA_WIDTH+1:0] s_fp_rowc_sum, s_fp_rowg_sum;
   logic [DATA_WIDTH-1:0] s_rowc_sum, s_rowg_sum;
 
-  assign valid = (itrg == (Z.ng-1) & Z.n == R.n);
+  assign valid = (r_itrg == (Z.ng-1) & !itrg & Z.n == R.n);
 
   always_ff @(posedge clk_i, negedge rstn_i) begin : proc_itrn
     if(~rstn_i) begin
       itrn <= '0;
       itrr <= '0;
       itrg <= '0;
+      r_itrg <= '0;
+      r_fp_rowc_sum <= '0;
+      r_fp_rowg_sum <= '0;
     end
     else begin
       itrn <= (itrn < Z.n-1) ? itrn + 1 : '0;
-      itrr <= (itrn == Z.n-1) ? ((itrr < R.nr-1) ? itrr+1 : '0) : itrr;
-      itrg <= (itrn == Z.n-1 & itrr == R.nr-1) ? ((itrg < Z.ng-1) ? itrg+1 : '0) : itrg;
+      if(itrn == Z.n-1)
+      begin
+        itrr <= (itrr < R.nr-1) ? itrr + 1 : '0;
+        if(itrr == R.nr - 1)
+          itrg <= (itrg < Z.ng-1) ? itrg+1 : '0;
+      end
+
+      r_itrg <= itrg;
 
       r_fp_rowc_sum <= (itrn < Z.n-1) ? s_fp_rowc_sum : '0;
       r_fp_rowg_sum <= (itrn < Z.n-1) ? s_fp_rowg_sum : '0;
+    end
+  end
+
+  always_ff @(posedge clk_i, negedge rstn_i) begin : proc_out
+    if(!rstn_i)
+    begin
+
+      for(int i = 0; i < NRMAX; i++)
+      begin
+        OUT.c[i] <= '0;
+        for(int j = 0; j < NGMAX; j++)
+          OUT.G[i][j] <= '0;
+      end
+
+      for(int i = 0; i < NCMAX; i++)
+      begin
+        OUT.b[i] <= 0;
+        for(int j = 0; j < NGMAX; j++)
+          OUT.A[i][j] <= '0;
+      end
+    end
+    else
+    begin
+
+      if(itrn == Z.n - 1)
+      begin
+        /* R.mat*Z.c -> R.nr*Z.n clock cycles*/
+        OUT.c[itrr] <= s_rowc_sum;
+        
+        /* R.mat*Z.G -> R.nr*Z.n*Z.ng clock cycles*/
+        OUT.G[itrr][itrg] <= s_rowg_sum;
+      end
+      
+      /* OUT.A = Z.A*/
+      for(int i = 0; i < NCMAX; i++)
+        if(i < Z.nc)
+          for(int j = 0; j < NGMAX; j++)
+            if(j < Z.ng)
+              OUT.A[i][j] = Z.A[i][j];
+            else
+              OUT.A[i][j] = '0;
+          else
+            for(int j = 0; j < NGMAX; j++)
+              OUT.A[i][j] = '0;
+
+        /* OUT.b = Z.b*/
+        for(int i = 0; i < NCMAX; i++)
+          if(i < Z.nc)
+            OUT.b[i] = Z.b[i];
+          else
+            OUT.b[i] = '0;
+
     end
   end
 
@@ -102,51 +164,6 @@ module linear_image #(
     OUT.n  = R.nr;
     OUT.ng = Z.ng;
     OUT.nc = Z.nc;
-
-    /* R.mat*Z.c -> R.nr*Z.n clock cycles*/
-    for(int i = 0; i < NRMAX; i++)
-      if(i < R.nr)
-        OUT.c[i] = (i == itrr) ? ((itrn == Z.n-1) ? s_rowc_sum : OUT.c[i]) : OUT.c[i];
-      else
-        OUT.c[i] = '0;
-    
-    /* R.mat*Z.G -> R.nr*Z.n*Z.ng clock cycles*/
-    for(int i = 0; i < NRMAX; i++)
-    begin
-      if(i < R.nr)
-      begin
-        for(int j = 0; j < NGMAX; j++)
-        begin
-          if(j < Z.ng)
-            OUT.G[i][j] = (i == itrr) ? ((j == itrg) ? ((itrn == Z.n-1) ? s_rowg_sum : OUT.G[i][j]) : OUT.G[i][j]) : OUT.G[i][j];
-          else
-            OUT.G[i][j] = '0;
-        end
-      end
-      else
-        for(int j = 0; j < NGMAX; j++)
-          OUT.G[i][j] = '0;
-    end
-    //OUT.G[itrr][itrg] = (itrn == Z.n-1) ? s_rowg_sum : OUT.G[itrr][itrg];
-
-    /* OUT.A = Z.A*/
-    for(int i = 0; i < NCMAX; i++)
-      if(i < Z.nc)
-        for(int j = 0; j < NGMAX; j++)
-          if(j < Z.ng)
-            OUT.A[i][j] = Z.A[i][j];
-          else
-            OUT.A[i][j] = '0;
-      else
-        for(int j = 0; j < NGMAX; j++)
-          OUT.A[i][j] = '0;
-
-    /* OUT.b = Z.b*/
-    for(int i = 0; i < NCMAX; i++)
-      if(i < Z.nc)
-        OUT.b[i] = Z.b[i];
-      else
-        OUT.b[i] = '0;
   end
 
 endmodule
